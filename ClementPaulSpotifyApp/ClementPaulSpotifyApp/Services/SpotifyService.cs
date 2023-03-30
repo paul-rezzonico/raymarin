76a3af1b1d8c23b724942f1395fbbf5c1d25be24
Paul Rezzonico <paul.rezzo@gmail.com>
2023-03-30 10:17:55 +0200

    add spotify nuggets and code

--- ClementPaulSpotifyApp/ClementPaulSpotifyApp/Services/SpotifyService.cs
+++ ClementPaulSpotifyApp/ClementPaulSpotifyApp/Services/SpotifyService.cs
@@ -1,35 +1,33 @@
-// using System;
-// using System.Threading.Tasks;
-// using SpotifyAPI.Web;
-//
-// namespace EliseSpotifyApp.Service
-// {
-//     public class SpotifyService
-//     {
-//         const string TokenSpotify = "MON_TOKEN";
-//         private SpotifyClient _spotifyClient;
-//         #region Instance
-//
-//         public static SpotifyService Instance { get; } = new SpotifyService();
-//
-//         #endregion
-//
-//         public async Task<bool> ConnectSpotify()
-//         {
-//             try
-//             {
-//                 _spotifyClient = new SpotifyClient(TokenSpotify);
-//                 return await Task.FromResult(true);
-//             }
-//             catch (Exception exception)
-//             {
-//                 return await Task.FromResult(false);
-//             }
-//         }
-//
-//         public SpotifyClient GetSpotifyClient()
-//         {
-//             return _spotifyClient;
-//         }
-//     }
-// }
+using System;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+namespace ClementPaulSpotifyApp.Service
+{
+    public class SpotifyService
+    {
+        const string TokenSpotify = "MON_TOKEN";
+        private SpotifyClient _spotifyClient;
+        #region Instance
+        public static SpotifyService Instance { get; } = new SpotifyService();
+        #endregion
+
+        public async Task<bool> ConnectSpotify()
+        {
+            try
+            {
+                _spotifyClient = new SpotifyClient(TokenSpotify);
+                return await Task.FromResult(true);
+            }
+            catch (Exception)
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public SpotifyClient GetSpotifyClient()
+        {
+            return _spotifyClient;
+        }
+    }
+}
